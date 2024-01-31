c4c94a2e34742d9b435ff9be0e26bec857674619
Camila <castrillonosorio12@gmail.com>
2024-01-31 13:04:23 -0500

    The title of the tacks table was edited in artist-details

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiUrl:'https://api.spotify.com/v1',
   client_id:'f4853540154f4fd7a436700c7fbbe206',
   client_secret: 'c244df8e533044e487b8fcfa88c7462f',
-  apiToken:'BQD2HsSWWdOYxmLtBbCjlHkul52rjtlQekikkNgdE3xDa6bXAfIfsT70D5tn3Vt3wQ1K4a6QHwA7iqtaDER_o7mjmHAkhDGIYJO0Xf6Wc9Dbss3jl7o'
+  apiToken:'BQDt2McrjMUNM1-hLeO62BEQ7Iay6s3vA7dXtxBI-4vE3tmDVcmdxZI8yeR8K3m2nJFCd8hPgLgbaLzaVIZzDJzsxxBPeFhugixG6nsmuvN9-w-IOo8'
 
 };
